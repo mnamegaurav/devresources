1bc18793e7bd4a7b2a5f9638e991659c7e629f73
Gaurav Sharma <gaurav91297@gmail.com>
2021-03-14 23:25:10 +0530

    added robots.txt and also optimized for seo

--- devresources/urls.py
+++ devresources/urls.py
@@ -17,9 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('devjunction/devresources/gaurav/admin/', admin.site.urls),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt", 
+            content_type="text/plain"
+        ),
+    ),
     path('accounts/', include('accounts.urls')),
     path('', include('core.urls')),
 ]
