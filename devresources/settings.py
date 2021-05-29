4a19e56ceab1ac57c4c7e8a6c965edb4bf7ff041
Gaurav Sharma <gaurav91297@gmail.com>
2021-05-30 00:31:11 +0530

    adde new requiremnets and settings

--- devresources/settings.py
+++ devresources/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
